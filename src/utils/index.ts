e261a4e2561b6e8e68c9779c3d14d78fbbe07a34
aman035 <guptaaman200115@gmail.com>
2024-09-09 18:16:20 +0530

    remove: unused code

--- src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export const getRandomElement = <T>(array: T[]): T => {
-  if (array.length === 0) {
-    throw new Error('Array cannot be empty')
-  }
-  const randomIndex = Math.floor(Math.random() * array.length)
-  return array[randomIndex]
-}
