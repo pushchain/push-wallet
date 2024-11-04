3f8ef76f0ee0ad614014b2030fafb4189e6b0d76
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-11-04 21:06:01 +0530

    removed localhost check

--- src/common/Common.appConfig.ts
+++ src/common/Common.appConfig.ts
@@ -1,18 +1,16 @@
 export function getAppBasePath() {
   if (typeof import.meta !== "undefined" && import.meta.env) {
     // Vite environment
-    return import.meta.env.VITE_DEV_MODE
-      ? import.meta.env.VITE_DEV_MODE === "localhost"
-        ? "/"
-        : "/push-wallet/"
-      : "/";
+    if (import.meta.env.VITE_DEV_MODE === "preview")
+      return "/push-wallet/pr-preview/"; // add support for pr number
+
+    if (import.meta.env.VITE_DEV_MODE === "testing") return "/push-wallet/";
   } else if (typeof process !== "undefined" && process.env) {
     // Node.js environment (e.g., during the build process)
-    return process.env.VITE_DEV_MODE
-      ? process.env.VITE_DEV_MODE === "localhost"
-        ? "/"
-        : "/push-wallet/"
-      : "/";
+    if (process.env.VITE_DEV_MODE === "preview")
+      return "/push-wallet/pr-preview/"; // add support for pr number
+
+    if (process.env.VITE_DEV_MODE === "testing") return "/push-wallt/";
   }
-  return "";
+  return "/";
 }
