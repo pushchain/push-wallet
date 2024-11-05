d1c9389f0f36f768ef981d1c161afa1b58d1f7a4
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-11-05 14:05:00 +0530

    Updates

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-// vitest.config.ts
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-  test: {
-    testTimeout: 15000,
-    environment: 'happy-dom', // Use happy-dom to simulate the browser environment,
-  },
-})
