29fc49d11f88793eb18888050b2513faa87b25c8
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-11-29 12:48:58 +0530

    added border color

--- src/common/components/BoxLayout.tsx
+++ src/common/components/BoxLayout.tsx
@@ -1,23 +1,43 @@
 import { FC, ReactNode } from "react";
 import { Box } from "../../blocks";
 import React from "react";
+import { css } from "styled-components";
 
 type BoxLayoutProps = {
   children: ReactNode;
 };
 
-const BoxLayout: FC<BoxLayoutProps> = ({children}) => {
+const BoxLayout: FC<BoxLayoutProps> = ({ children }) => {
   return (
     <Box
       alignItems="center"
-      backgroundColor="surface-primary"
       display="flex"
       flexDirection="column"
       borderRadius="radius-md"
-      border="border-xmd solid stroke-secondary"
       justifyContent="center"
-      width= "auto"
-    >{children}
+      width="auto"
+      css={css`
+        background: linear-gradient(
+          160deg,
+          #313338 0 25%,
+          #d548ec,
+          #0056d0,
+          #313338 75% 100%
+        );
+        padding: 1.5px;
+      `}
+    >
+      <Box
+        backgroundColor="surface-primary"
+        alignItems="center"
+        display="flex"
+        flexDirection="column"
+        borderRadius="radius-md"
+        justifyContent="center"
+        width="auto"
+      >
+        {children}
+      </Box>
     </Box>
   );
 };
