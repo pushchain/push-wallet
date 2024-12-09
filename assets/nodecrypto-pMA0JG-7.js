d63563f3516666285bdba9d6fa07518a00bfe9f3
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-12-09 16:15:55 +0530

    Updates

--- assets/nodecrypto-pMA0JG-7.js
+++
@@ -1,1 +0,0 @@
-import{r as n,B as y}from"./index-CkcpbNxk.js";import{c as p}from"./utils-B7bt9iXy.js";var r=n();const m=async t=>{const{content:o,publicKey:i,privateKey:s}=t,c=r.createPrivateKey({key:p({uncompressedPrivateKeyHex:s,compressedPublicKeyHex:i}),format:"jwk"}),e=r.createSign("SHA256");return e.write(y.from(o)),e.end(),e.sign(c,"hex")};export{m as signWithApiKey};
