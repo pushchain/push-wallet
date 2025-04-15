77682d98809e4d6b34bbfb2a551be9eadbfa3a18
abhishek-01k <abhishek@push.org>
2025-04-15 11:35:47 +0530

    Removed push chain package

--- src/queries/models/getUserTransactionsModelCreator.ts
+++
@@ -1,7 +0,0 @@
-import { BlockResponse } from '@pushprotocol/push-chain/src/lib/block/block.types';
-
-
-//add type
-//any remodelling needed in the response can be done here
-export const getUserTransactionsModelCreator = (response: BlockResponse): any =>
-  response;
